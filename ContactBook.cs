74467b072cdff58f1b639b26550763e150aec978
singhtushar689 <singhtushar689@gmail.com>
2022-11-24 22:23:40 +0530

    Address Book

---
+++ ContactBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class ContactBook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string MobileNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+             bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to AddressBook");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Mobilenumber, Email");
+                        ContactBook contact = new ContactBook()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            MobileNumber= Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
+        
     }
 }
